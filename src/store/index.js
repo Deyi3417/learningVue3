f191566cf29819640913528c679af1cce41716a9
Deyi3417 <18811553417@163.com>
2022-08-15 01:23:32 +0800

    前端通过代理从后端拿到数据

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-// 状态的集中管理
+// 状态的集中管理--数据集中放到这里进行管理
 // 1. 数据实现响应式
 // ref reactive --->对象中存储这状态message,age,counter
 
@@ -7,13 +7,21 @@ import {reactive} from "vue";
 const store = {
     state: reactive({
         name: "liudy23",
-        age: 18
+        age: 18,
+        // 定义一个空的数组
+        userList: []
     }),
 
     // 定义一个函数
     updateAge: function () {
         this.state.age = 9
     },
+
+    updateUserList(value) {
+        this.state.userList = value;
+        console.log("--已执行updateUserList函数--")
+        console.log(this.state.userList)
+    }
 }
 
 // 2. 如何再APP组件中通过provide提供
--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -1,11 +1,15 @@
 <script>
 import axios from "axios";
+import store from "../../store";
 
 export default {
   name: "User",
+  inject: ['store'],
 
-  setup() {
-
+  data(){
+    return{
+      store
+    }
   },
   created() {
     console.log("---liudy23测试----")
@@ -16,6 +20,13 @@ export default {
       console.log(res.data.data)
       console.log(res.data.code)
       console.log(res.data.message)
+      return res.data;
+    }).then((res) => {
+      // 将data存储到仓库
+      console.log("----后台返回的数据封装到：res----")
+      console.log(res)
+      // store.state.userList =res.data;
+      store.updateUserList(res.data)
     })
   }
 
@@ -28,6 +39,14 @@ export default {
     <div>
       <h2>User 界面</h2>
     </div>
+    <div>
+      <ul>
+        <li v-for="item in store.state.userList" :key="item.id">
+          {{item.username}}--{{item.userAccount}}
+        </li>
+      </ul>
+    </div>
+
   </div>
 
 </template>
